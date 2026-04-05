5d193b406562b55eca8534339458ad094c5a9f37
kn1ghtOwl <vinayak03.2003@gmail.com>
2026-04-05 14:25:57 +0530

    created add new user route

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,6 @@
 import z from "zod";
 import db from "../db/database.js";
+import bcrypt from "bcryptjs";
 
 const CreateUserSchema = z.object({
     name: z.string().min(1),
@@ -25,6 +26,36 @@ const getUsers = (req, res) => {
         res.status(401).json({message: error.message});
         console.log("Error in getUsers: ", error.message);
     }
+};
+
+const addUsers = async (req, res) => {
+    try {
+        const result = CreateUserSchema.safeParse(req.body);
+
+        if(!result.success){
+            return res.status(400).json({message: "Validation Failed!!"})
+        };
+
+        const {name, email, password, role} = result.data;
+        const hashPassword = await bcrypt.hash(password, 10);
+
+        const userExist = db.prepare('Select * from Users where email = ?').get(email);
+        if(userExist){
+            return res.status(400).json({message: "User already exists!"})
+        };
+
+        const newUser = db.prepare('Insert into Users (name, email, password, role) Values(?, ?, ?, ?)').run(name, email, hashPassword, role);
+        const newUserDate = db.prepare('Select id, name, email, role, status from Users where id = ?').get(newUser.lastInsertRowid);
+
+        res.status(201).json({
+            message: "New user Created!",
+            user: newUserDate
+        })
+
+    } catch (error) {
+        res.status(500).json({message: error.message});
+        console.log("Error in addUsers: ", error.message);
+    }
 }
 
-export {getUsers};
+export {getUsers, addUsers};
--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import authMiddleware from "../middleware/authMiddleware.js";
 import roleAccess from "../middleware/roleMiddleware.js";
-import { getUsers } from "../controllers/userController.js";
+import { getUsers, addUsers } from "../controllers/userController.js";
 
 const router = Router();
 
 router.use(authMiddleware, roleAccess('admin'));
 
 router.get("/", getUsers);
+router.post("/", addUsers);
 
 export default router;
 
